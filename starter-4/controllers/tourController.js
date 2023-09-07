513eabfff4d689f88cab693b58c708f9cb969205
abdul-nishar <sheikhabdul285@gmail.com>
2023-09-07 23:23:39 +0530

    STARTER-4

---
+++ starter-4/controllers/tourController.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
+);
+
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is : ${val}`);
+  if (req.params.id > tours.length) {
+    return res.status(404).json({
+      status: '404 Not Found',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: '400 Bad Request',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+};
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((element) => element.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+exports.updateTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour...>',
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  const newTours = tours.filter((tour) => tour.id !== +req.params.id);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(newTours),
+    () => {
+      res.status(204).json({
+        status: 'success',
+        data: null,
+      });
+    },
+  );
+};
+
+exports.createTour = (req, res) => {
+  const newId = tours.length;
+  const newTour = Object.assign(req.body, { id: newId });
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    () => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
+};
